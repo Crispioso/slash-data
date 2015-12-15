46d7d0b4b5dd18bcf7dc1ab2ef8eb38ff881f2c2
Crispioso <crispin.merriman@gmail.com>
2015-12-15 22:02:26 +0000

    Use commands api to detect keyboard input

--- slash-data.js
+++ slash-data.js
@@ -1,28 +1,22 @@
-$(function() {
-    var keys = [];
-    $(document).keydown(function(e) {
-        keys[e.keyCode] = true;
-    }).keyup(function(e) {
-        if (keys[16] && keys[17] && keys[85]) {
-            var path = window.location.protocol + "//" + window.location.host + window.location.pathname,
-                params = window.location.search,
-                data = "data";
-
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    var newCommand = request.command;
+    console.log("Received command: " + newCommand);
+    
+    if (newCommand === "json") {
+        var path = window.location.protocol + "//" + window.location.hostname + window.location.pathname;
+            var params = window.location.search;
+            var data = "data";
+            
             if (!path.match("/$")) {
                 data = "/data";
             }
             if (!path.match("data")) {
-                console.log(path);
-                console.log(data);
-                console.log(params);
                 window.location.href = path + data + params;
             } else {
                 path = path.replace("/data", "/");
-                console.log(path);
-                console.log(params);
                 window.location.href = path + params;
             }
-        }
-        keys[e.keyCode] = false;
-    });
+    }
+    
+    sendResponse({"action": "Actioned trigger: " + newCommand});
 });
