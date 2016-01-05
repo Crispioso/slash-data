8487bdf9ca4365afc232ade649e95d70caf4fc65
Crispin Merriman <crispin.merriman@gmail.com>
2016-01-05 22:36:55 +0000

    Fix endpoint bugs

--- slash-data.js
+++ slash-data.js
@@ -1,8 +1,19 @@
+// function checkSlash(str) {
+//     if(!path.match("/$")) {
+//         str = "/" + str;
+//     }
+//     console.log(str);
+// }
+
 var taxonomy = ["businessindustryandtrade", "business", "activitysizeandlocation", "businessinnovation", "businessservices", "changestobusiness", "bankruptcyinsolvency", "businessbirthsdeathsandsurvivalrates", "mergersandacquisitions", "constructionindustry", "internationaltrade", "itandinternetindustry", "manufacturingandproductionindustry", "retailindustry", "tourismindustry", "economy", "economicoutputandproductivity", "output", "productivitymeasures", "publicservicesproductivity", "environmentalaccounts", "governmentpublicsectorandtaxes", "localgovernmentfinance", "publicsectorfinance", "publicspending", "researchanddevelopmentexpenditure", "taxesandrevenue", "grossdomesticproductgdp", "grossvalueaddedgva", "inflationandpriceindices", "investmentspensionsandtrusts", "nationalaccounts", "balanceofpayments", "satelliteaccounts", "supplyandusetables", "uksectoraccounts", "regionalaccounts", "grossdisposablehouseholdincome", "employmentandlabourmarket", "peopleinwork", "earningsandworkinghours", "employmentandemployeetypes", "labourproductivity", "workplacedisputesandworkingconditions", "workplacepensions", "peoplenotinwork", "unemployment", "publicsectorpersonnel", "peoplepopulationandcommunity", "birthsdeathsandmarriages", "adoption", "ageing", "conceptionandfertilityrates", "deaths", "divorce", "families", "lifeexpectancies", "livebirths", "marriagecohabitationandcivilpartnerships", "maternities", "stillbirths", "community", "cohesionandparticipation", "neighbourhoods", "crimeandjustice", "culturalidentity", "ethnicity", "language", "religion", "sexuality", "educationandchildcare", "elections", "electoralregistration", "generalelections", "localgovernmentelections", "healthandsocialcare", "causesofdeath", "childhealth", "conditionsanddiseases", "disability", "drugusealcoholandsmoking", "healthcaresystem", "healthinequalities", "healthandlifeexpectancies", "healthandwell-being", "mentalhealth", "socialcare", "householdcharacteristics", "homeinternetandsocialmediausage", "housing", "leisureandtourism", "personalandhouseholdfinances", "debt", "expenditure", "incomeandwealth", "pensionssavingsandinvestments", "populationandmigration", "internationalmigration", "migrationwithintheuk", "populationestimates", "populationprojections", "wellbeing"];
 
-var path = window.location.protocol + "//" + window.location.host + window.location.pathname,
+var endpoints = ["datalist", "publications"];
+
+var path = window.location.pathname,
+    domain = window.location.protocol + "//" + window.location.host,
     urlEnd = path.match('[^/]+(?=/$|$)'),
-    data = "data";
+    data = "data",
+    atRoot = false;
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     var newCommand = request.command;
@@ -12,24 +23,30 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         data = "/data";
     }
     
-    if (newCommand === "json") {
+    if (newCommand === "json") { // Toggle JSON endpoint
         var params = window.location.search;
-        if (urlEnd[0] !== 'data') {
-            window.location.href = path + data + params;
+        if (!urlEnd || urlEnd[0] !== 'data') {
+            window.location.href = domain + path + data + params;
         } else {
             var lastSlash = path.lastIndexOf("/");
             path = path.substring(0,lastSlash);
-            window.location.href = path + params;
+            window.location.href = domain + path + params;
         }
-    } else if (newCommand === "publications" || newCommand === "datalist") {
-        if (!path.match("/$")) {
+    } else if (endpoints.indexOf(newCommand) > -1) { // Toggle list page endpoints
+        if (!path.match("/$")) { // Add slash if url doesn't end in one
             newCommand = "/" + newCommand;
         }
-        if (taxonomy.indexOf(urlEnd[0]) > -1) {
-            window.location.href = path + newCommand;
-        } else if (newCommand.indexOf(urlEnd[0])) {
+        if (!urlEnd || urlEnd[0] === "search") { // Add endpoint to absolute root
+            window.location.href = domain + "/" + newCommand;
+            console.log(path);
+        } else if (taxonomy.indexOf(urlEnd[0]) > -1 && newCommand.indexOf(urlEnd[0]) == -1) { // Toggle endpoints if on normal taxonomy page
+            window.location.href = domain + path + newCommand;
+        } else if (newCommand.indexOf(urlEnd[0]) > -1 && !atRoot) { // Remove endpoint if matches current command - exclude root/search page
             path = path.replace(newCommand, '');
-            window.location.href = path;
+            window.location.href = domain + path;
+        } else if (endpoints.indexOf(urlEnd[0]) > -1) { // Allow toggle of other end point if already on list page
+            path = path.replace("/" + urlEnd[0], newCommand);
+            window.location.href = domain + path;
         }
     }
     
